7144c87d5f2d8831c0da6641c2dcb649045b473e
Eduard Firut <edi@openline.ai>
2022-11-04 09:18:50 +0200

    improve base layout

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,43 +1,23 @@
-import Header from "./header";
 import LayoutMenu from "./menu";
 import {useRouter} from "next/router";
 
 export default function Layout({children}: any) {
     const router = useRouter();
 
-
     return (
-        <>
-            <Header height={'70px'}/>
+            <div className="flex h-full w-full">
 
-            <div className="flex" style={{height: 'calc(100vh - 90px)'}}>
+                <div className="flex-grow-0 flex h-full" style={{width: '200px'}}>
 
-                <div className="flex-grow-0 flex"
-                     style={{width: '200px', height: '100%'}}>
-
-                    <div style={{
-                        width: '100%',
-                        height: '100%',
-                        padding: '10px 0px 10px 10px',
-                        overflow: 'hidden'
-                    }}>
-
-                        <LayoutMenu/>
-
-                    </div>
+                    <LayoutMenu/>
 
                 </div>
 
-                <div className="flex-grow-1 flex" style={{height: '100%'}}>
-                    <div style={{
-                        width: '100%',
-                        height: '100%',
-                        margin: '10px'
-                    }}>
+                <div className="flex-grow-1 flex h-full">
+                    <div className="w-full h-full">
                         {children}
                     </div>
                 </div>
             </div>
-        </>
     )
 }
