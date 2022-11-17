3ecdfe670c7f6ef67d9d9a5a5dbbcecfd48730cb
Eduard Firut <edi@openline.ai>
2022-11-17 10:24:07 +0200

    Contact companies

---
+++ contacts-gui/pages/contact/contactCompaniesPositions.tsx
@@ -0,0 +1,133 @@
+import PropTypes, {string} from "prop-types";
+import {useEffect, useState} from "react";
+import {gql, GraphQLClient} from "graphql-request";
+import {Button} from "primereact/button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCirclePlus} from "@fortawesome/free-solid-svg-icons";
+import {uuidv4} from "../../utils/uuid-generator";
+import ContactCompanyPositionTemplate from "./contactCompanyPositionTemplate";
+
+function ContactCompaniesPositions(props: any) {
+    const client = new GraphQLClient(`${process.env.API_PATH}/query`);
+
+    const [companyPositions, setCompanyPositions] = useState([] as any);
+
+    useEffect(() => {
+
+        if (props.contactId !== undefined) {
+
+            const query = gql`query LoadCompaniesForContactWithId($id: ID!) {
+                contact(id: $id) {
+                    companyPositions{
+                        id
+                        company{
+                            id
+                            name
+                        }
+                        jobTitle
+                    }
+                }
+            }`
+
+            client.request(query, {id: props.contactId}).then((response: any) => {
+                response.contact.companyPositions.forEach((e: any) => {
+                    e.uiKey = uuidv4(); //TODO make sure the ID is unique in the array
+                    e.newItem = false;
+                    e.companyId = e.company.id;
+                    e.companyName = e.company.name;
+                });
+                setCompanyPositions(response.contact.companyPositions);
+            });
+        }
+
+    }, [props.contactId]);
+
+    const companyPositionSaved = (data: any) => {
+        setCompanyPositions(companyPositions.map((e: any) => {
+            if (e.uiKey !== data.uiKey) {
+                //if the saved company position is marked as primary, we unmark the others
+                return e;
+            } else {
+                return {...data, ...{newItem: false}};
+            }
+        }));
+    }
+
+    const companyPositionDeleted = (uiKey: string) => {
+        setCompanyPositions(companyPositions.filter((e: any) => {
+            if (e.uiKey !== uiKey) {
+                return e;
+            }
+        }));
+    }
+
+    const companyPositionCancelEdit = (uiKey: string) => {
+        setCompanyPositions(companyPositions.filter((e: any) => {
+            if (e.uiKey !== uiKey) {
+                return e;
+            } else {
+                if (!e.newItem) {
+                    return e;
+                }
+            }
+        }));
+    }
+
+    return (
+        <div className='card-fieldset mt-3' style={{width: '25rem'}}>
+            <div className="card-header">
+                <div className="flex flex-row w-full">
+                    <div className="flex-grow-1">Companies</div>
+                    <div className="flex">
+
+                        <Button className="p-button-text p-0" onClick={(e: any) => {
+                            setCompanyPositions([...companyPositions, {
+                                id: undefined,
+                                companyId: undefined,
+                                companyName: '',
+                                jobTitle: '',
+                                uiKey: uuidv4(), //TODO make sure the ID is unique in the array
+                                newItem: true // this is used to remove the item from the company positions array in case of cancel new item
+                            }]);
+                        }}>
+                            <FontAwesomeIcon size="xs" icon={faCirclePlus} style={{color: 'black'}}/>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            <div className="card-body">
+
+                {
+                    companyPositions.length === 0 &&
+                    <div className="display">
+                        No companies associated
+                    </div>
+                }
+
+                {
+                    companyPositions.map((e: any) => {
+                        return <ContactCompanyPositionTemplate key={e.uiKey}
+                                                               contactId={props.contactId}
+                                                               companyPosition={e}
+                                                               initialEditState={e.newItem}
+                                                               notifySave={(e: any) => companyPositionSaved(e)}
+                                                               notifyDelete={(uiKey: string) => companyPositionDeleted(uiKey)}
+                                                               notifyCancelEdit={(uiKey: string) => companyPositionCancelEdit(uiKey)}
+                        />
+                    })
+                }
+
+            </div>
+        </div>
+    );
+}
+
+ContactCompaniesPositions.propTypes = {
+    contactId: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(string)
+    ]) || undefined,
+    className: PropTypes.arrayOf(string)
+}
+
+export default ContactCompaniesPositions
---
+++ contacts-gui/pages/contact/contactCompanyPositionTemplate.tsx
@@ -0,0 +1,264 @@
+import {Controller, useForm} from "react-hook-form";
+import PropTypes from "prop-types";
+import {useState} from "react";
+import {gql, GraphQLClient} from "graphql-request";
+import {Button} from "primereact/button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEdit, faTrashCan} from "@fortawesome/free-solid-svg-icons";
+import {Dialog} from "primereact/dialog";
+import {InputText} from "primereact/inputtext";
+import SearchOrAddComponent from "../../components/SearchOrAddComponent";
+
+function ContactCompanyPositionTemplate(props: any) {
+    const client = new GraphQLClient(`${process.env.API_PATH}/query`);
+
+    const {register, setValue, handleSubmit, control} = useForm({
+        defaultValues: props.companyPosition
+    });
+
+    const onSubmit = handleSubmit((data: any) => {
+
+        let query = undefined;
+
+        if (!data.id) {
+            query = gql`mutation MergeCompanyPosition ($contactId: ID!, $companyPosition: CompanyPositionInput!) {
+                contact_MergeCompanyPosition(contactId: $contactId, input: $companyPosition){
+                    id
+                    company{
+                        id
+                        name
+                    }
+                    jobTitle
+                }
+            }`
+        } else {
+            query = gql`mutation UpdateCompanyPosition ($contactId: ID!, $companyPositionId: ID!, $companyPosition: CompanyPositionInput!) {
+                contact_UpdateCompanyPosition(contactId: $contactId, companyPositionId: $companyPositionId input: $companyPosition){
+                    id
+                    company{
+                        id
+                        name
+                    }
+                    jobTitle
+                }
+            }`
+        }
+
+        client.request(query, {
+            contactId: props.contactId,
+            companyPositionId: data.id,
+            companyPosition: {
+                company: {
+                    id: data.companyId,
+                    name: data.companyName
+                },
+                jobTitle: data.jobTitle
+            }
+        }).then((response) => {
+                if (!data.id) {
+                    props.notifySave({
+                        ...response.contact_MergeCompanyPosition, ...{
+                            uiKey: data.uiKey,
+                            companyId: response.contact_MergeCompanyPosition.company.id,
+                            companyName: response.contact_MergeCompanyPosition.company.name
+                        }
+                    });
+                } else {
+                    props.notifySave({
+                        ...response.contact_UpdateCompanyPosition, ...{
+                            uiKey: data.uiKey,
+                            companyId: response.contact_UpdateCompanyPosition.company.id,
+                            companyName: response.contact_UpdateCompanyPosition.company.name
+                        }
+                    });
+                }
+                setEditDetails(false);
+            }
+        ).catch((reason) => {
+            if (reason.response.status === 400) {
+                // reason.response.data.errors.forEach((error: any) => {
+                //     formik.setFieldError(error.field, error.message);
+                // })
+                //todo show errors on form
+            } else {
+                alert('error');
+            }
+        });
+
+    })
+
+    const [deleteCompanyPositionConfirmationModalVisible, setDeleteCompanyPositionConfirmationModalVisible] = useState(false);
+    const deleteCompanyPosition = () => {
+        //todo show loading
+        const query = gql`mutation DeleteCompanyPosition ($contactId: ID!, $companyPositionId: ID!) {
+            contact_DeleteCompanyPosition(contactId: $contactId, companyPositionId: $companyPositionId){
+                result
+            }
+        }`
+
+        client.request(query, {
+            contactId: props.contactId,
+            companyPositionId: props.companyPosition.id
+        }).then((response: any) => {
+            if (response.contact_DeleteCompanyPosition.result) {
+                //todo show notification
+
+                props.notifyDelete(props.companyPosition.uiKey);
+
+                setDeleteCompanyPositionConfirmationModalVisible(false);
+            } else {
+                //TODO throw error
+            }
+        });
+    }
+    const notifyCancelEdit = (uiKey: string) => {
+        setEditDetails(false);
+        props.notifyCancelEdit(uiKey);
+    }
+
+    const [editDetails, setEditDetails] = useState(props.initialEditState ?? false);
+
+    const searchCompany = function (name: string, maxResults: string) {
+        return new Promise((resolve, reject) => {
+
+            const query = gql`query SearchCompanyByName ($pagination: PaginationFilter!, $name: String!) {
+                companies_ByNameLike(paginationFilter: $pagination, companyName: $name){
+                    content{
+                        id
+                        name
+                    }
+                    totalPages
+                    totalElements
+                }
+            }`
+
+            client.request(query, {
+                pagination: {
+                    "page": 0,
+                    "limit": 25
+                },
+                name: name
+            }).then((response: any) => {
+                if (response.companies_ByNameLike.content) {
+                    resolve({
+                        content: response.companies_ByNameLike.content,
+                        totalElements: response.companies_ByNameLike.totalElements
+                    });
+                } else {
+                    resolve({
+                        error: response
+                    });
+                }
+            });
+        });
+    }
+
+    return (
+        <>
+            {
+                !editDetails &&
+                <div className="display">
+                    <div className="grid grid-nogutter mt-3">
+                        <div className="col-8">
+                            Company: {props.companyPosition.companyName}<br/>
+                            Job: {props.companyPosition.jobTitle}
+                        </div>
+                        <div className="col-4">
+
+                            <Button className="p-button-text p-0" onClick={(e: any) => {
+                                setValue('id', props.companyPosition.id);
+                                setValue('companyId', props.companyPosition.companyId);
+                                setValue('companyName', props.companyPosition.companyName);
+                                setValue('companyNameAutocomplete', {
+                                    companyId: props.companyPosition.companyId,
+                                    companyName: props.companyPosition.companyName
+                                });
+                                setValue('jobTitle', props.companyPosition.jobTitle);
+                                setEditDetails(true);
+                            }}>
+                                <FontAwesomeIcon size="xs" icon={faEdit} style={{color: 'black'}}/>
+                            </Button>
+
+                            <Button className="p-button-text p-0" onClick={(e: any) => setDeleteCompanyPositionConfirmationModalVisible(true)}>
+                                <FontAwesomeIcon size="xs" icon={faTrashCan} style={{color: 'black'}}/>
+                            </Button>
+                            <Dialog header="Company position delete confirmation"
+                                    draggable={false}
+                                    visible={deleteCompanyPositionConfirmationModalVisible}
+                                    footer={
+                                        <div className="flex flex-grow-1 justify-content-between align-items-center">
+                                            <Button label="Delete the company position" icon="pi pi-check" onClick={() => deleteCompanyPosition()} autoFocus/>
+                                            <Button label="Cancel" icon="pi pi-times" onClick={() => setDeleteCompanyPositionConfirmationModalVisible(false)} className="p-button-text"/>
+                                        </div>
+                                    }
+                                    onHide={() => setDeleteCompanyPositionConfirmationModalVisible(false)}>
+                                <p>Please confirm that you want to delete this company position.</p>
+                            </Dialog>
+
+                        </div>
+                    </div>
+                </div>
+            }
+
+            {
+                editDetails &&
+                <div className="content">
+                    <form>
+                        <div className="field w-full">
+                            <label htmlFor="companyName" className="block">Company *</label>
+                            <Controller name="companyName" control={control} render={({field}) => (
+                                <SearchOrAddComponent
+                                    value={field.value}
+                                    searchData={(name: string, maxResults: string) => {
+                                        return searchCompany(name, maxResults);
+                                    }}
+                                    searchItemTemplate={(e: any) => {
+                                        return <span className="mr-3">{e.name}</span>
+                                    }}
+                                    onInputValueChanged={(c: any) => {
+                                        setValue('companyId', undefined);
+                                        setValue('companyName', c.label);
+                                    }}
+                                    onItemSelected={(e: any) => {
+                                        setValue('companyId', e.id);
+                                        setValue('companyName', e.name);
+                                    }}
+                                    maxResults={2}/>
+                            )}/>
+                        </div>
+                        <div className="field w-full">
+                            <label htmlFor="jobTitle" className="block">Job *</label>
+                            <InputText id="jobTitle" {...register("jobTitle")} className="w-full"/>
+                        </div>
+                    </form>
+
+                    <div className="flex justify-content-end">
+                        <Button onClick={(e: any) => notifyCancelEdit(props.companyPosition.uiKey)} className='p-button-link text-gray-600' label="Cancel"/>
+                        <Button onClick={() => onSubmit()} label="Save"/>
+                    </div>
+                </div>
+            }
+        </>
+    );
+}
+
+ContactCompanyPositionTemplate.propTypes = {
+    contactId: PropTypes.string,
+    companyPosition: PropTypes.object,
+    initialEditState: PropTypes.bool,
+    notifySave: PropTypes.func,
+    notifyDelete: PropTypes.func,
+    notifyCancelEdit: PropTypes.func
+}
+
+ContactCompanyPositionTemplate.defaultValues = {
+    initialEditState: false,
+    notifySave: () => {
+    },
+    notifyDelete: () => {
+    },
+    notifyCancelEdit: () => {
+    }
+}
+
+export default ContactCompanyPositionTemplate
