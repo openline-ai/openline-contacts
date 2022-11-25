70d53f119e7fef93de146f347c585e45b44982f2
Eduard Firut <edi@openline.ai>
2022-11-25 15:44:55 +0200

    asd

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
     API_PATH: process.env.API_PATH,
     TENANT_ID: process.env.TENANT_ID,
     CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   },
   output: 'standalone'
 }
