78202aed55342492ef3d581767911e08ded06b6c
Eduard Firut <edi@openline.ai>
2022-12-17 14:20:53 +0200

    Fix local deployment

--- packages/apps/contacts/next.config.js
+++ packages/apps/contacts/next.config.js
@@ -4,7 +4,16 @@ const nextConfig = {
   swcMinify: true,
   env: {
     CUSTOMER_OS_API_PATH: process.env.CUSTOMER_OS_API_PATH,
-    OASIS_GUI_PATH: process.env.OASIS_GUI_PATH
+    CUSTOMER_OS_API_KEY: process.env.CUSTOMER_OS_API_KEY,
+
+    OASIS_GUI_PATH: process.env.OASIS_GUI_PATH,
+
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_OAUTH_CLIENT_ID: process.env.NEXTAUTH_OAUTH_CLIENT_ID,
+    NEXTAUTH_OAUTH_CLIENT_SECRET: process.env.NEXTAUTH_OAUTH_CLIENT_SECRET,
+    NEXTAUTH_OAUTH_TENANT_ID: process.env.NEXTAUTH_OAUTH_TENANT_ID,
+    NEXTAUTH_OAUTH_SERVER_URL: process.env.NEXTAUTH_OAUTH_SERVER_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   output: 'standalone'
 }
