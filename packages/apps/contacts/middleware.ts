9d96ee279ce0f2994fa7486dd38ef732014e8128
edifirut <108661145+edifirut@users.noreply.github.com>
2023-01-14 00:27:30 +0200

    Fix webchat http requests to pass through proxy (#184)

--- packages/apps/contacts/middleware.ts
+++ packages/apps/contacts/middleware.ts
@@ -4,7 +4,7 @@ import {NextResponse} from "next/server";
 // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 
 export default withAuth(function middleware(request: NextRequestWithAuth) {
-        if (!request.nextUrl.pathname.startsWith('/customer-os-api/') && !request.nextUrl.pathname.startsWith('/fs/')) {
+        if (!request.nextUrl.pathname.startsWith('/customer-os-api/') && !request.nextUrl.pathname.startsWith('/fs/') && !request.nextUrl.pathname.startsWith('/ch/')) {
             return NextResponse.next()
         } else {
 
@@ -17,6 +17,9 @@ export default withAuth(function middleware(request: NextRequestWithAuth) {
             } else if (request.nextUrl.pathname.startsWith('/fs/')) {
                 newURL = process.env.FILE_STORAGE_API_PATH + "/" + request.nextUrl.pathname.substring(("/fs/").length);
                 requestHeaders.set('X-Openline-API-KEY', process.env.FILE_STORAGE_API_KEY as string)
+            } else if (request.nextUrl.pathname.startsWith('/ch/')) {
+                newURL = process.env.WEB_CHAT_HTTP_PATH + "/" + request.nextUrl.pathname.substring(("/ch/").length);
+                requestHeaders.set('WebChatApiKey', process.env.WEB_CHAT_API_KEY as string)
             }
 
             if (request.nextUrl.searchParams) {
@@ -44,5 +47,5 @@ export default withAuth(function middleware(request: NextRequestWithAuth) {
     })
 
 export const config = {
-    matcher: ['/customer-os-api/(.*)', '/fs/(.*)'],
+    matcher: ['/customer-os-api/(.*)', '/fs/(.*)', '/ch/(.*)'],
 }
