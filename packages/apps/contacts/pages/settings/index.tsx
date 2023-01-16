2eb5ff6578107d9a629eea5f8e233a97743a15ac
Kasia <121807793+Kasia9090@users.noreply.github.com>
2023-01-16 17:42:55 +0100

    (fix) Fix for demo (#197)

--- packages/apps/contacts/pages/settings/index.tsx
+++ packages/apps/contacts/pages/settings/index.tsx
@@ -29,9 +29,9 @@ const Settings: NextPage = () => {
             setAdminEmail(settings.zendeskAdminEmail || '')
 
         }).catch((reason: any) => {
-            if(reason.response.status !== 404) {
-                toast.error("There was a problem on our side and we cannot load settings data at the moment,  we are doing our best to solve it! ");
-            }
+            // if(reason.response.status !== 404) {
+            //     // toast.error("There was a problem on our side and we cannot load settings data at the moment,  we are doing our best to solve it! ");
+            // }
         });
     }, [])
 
