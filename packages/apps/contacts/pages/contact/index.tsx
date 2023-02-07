d7817b0e6bd9ed868d1f6aa9c29e5a074f2ddfa5
kasia <kasia@openline.ai>
2023-02-07 20:55:23 +0100

    feat: #311 New dashboard

--- packages/apps/contacts/pages/contact/index.tsx
+++ packages/apps/contacts/pages/contact/index.tsx
@@ -1,6 +1,6 @@
 import type {NextPage} from 'next'
 
-import {ContactList as ContactListComponent} from "../../components/contact/index";
+import {DashboardList as ContactListComponent} from "../../components/contact/index";
 
 const ContactList: NextPage = () => {
 
