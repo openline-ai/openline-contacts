58654849786ecd0217770cfc46e932c86b530e3f
kasia <kasia@openline.ai>
2023-02-07 21:00:20 +0100

    build fix

--- packages/apps/contacts/pages/organization/index.tsx
+++
@@ -1,9 +0,0 @@
-import type {NextPage} from 'next'
-import {OrganizationList as OrganizationListComponent} from '../../components/organization/OrganizationList'
-const OrganizationList: NextPage = () => {
-    return (
-        <OrganizationListComponent fullScreenMode />
-    );
-}
-
-export default OrganizationList
